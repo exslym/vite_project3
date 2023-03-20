ed3f2616efbef8bf8144ac50b0eae5ada51b7412
exslym <76844562+exslym@users.noreply.github.com>
2023-03-21 00:56:22 +0400

    fixed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,42 +1,18 @@
 import '../styles/index.scss';
-import { main } from './main/main';
-import menuBurger from './main/menuBurger';
-import mobileZoom from './main/mobileZoom';
-import modals from './main/modals';
-import resultPage from './main/resultPage';
-import { nextQuestion, testEndButton, testStartButton } from './portfolioTests/testOptions';
-import { firstTestBlock, portfolioTests } from './portfolioTests/tests';
 
-window.addEventListener('DOMContentLoaded', function () {
+import { elementAnimation } from './extra/animations';
+import { popup } from './extra/popup';
+import { smoothShowBlock } from './extra/showBlock';
+
+window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
-  // MAIN (START PAGE, MENU LINKS):
-  main();
+  /* SHOW/HIDE BLOCK */
+  smoothShowBlock('openButton', 'openBlock', '_active');
 
-  // MENU-BURGER:
-  menuBurger();
+  /* ZOOM for PLUS buttons on mobile devices */
+  popup('popup');
 
-  // MODALS:
-  modals();
-
-  // ZOOM IMAGES IN MOBILE DEVICES:
-  mobileZoom();
-
-  // START TEST BUTTON:
-  testStartButton();
-
-  // START FIRST TEST BLOCK:
-  firstTestBlock();
-
-  // NEXT QUESTION BUTTON:
-  nextQuestion();
-
-  // END TEST BUTTON:
-  testEndButton();
-
-  // RESULT PAGE:
-  resultPage();
-
-  // TESTS:
-  portfolioTests();
+  /*  ANIMATIONS */
+  elementAnimation('appearingBlock', '_animated2');
 });
--- src/scripts/main/mobileZoom.js
+++
@@ -1,8 +0,0 @@
-// import $ from 'jquery';
-// require('../../../tools/magnific-popup.min.js');
-
-export default function mobileZoom() {
-  if (document.querySelector('.popup')) {
-    $('.popup').magnificPopup();
-  }
-}
